3ae2c611a8de78cf11b5cbad87d4e772f502404a
xXxS37V3NxXx <mattso.rensen77@gmail.com>
2023-05-06 11:43:57 -0600

    Week 9 Coding Assignment

---
+++ src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,18 @@
+package projects.service;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+public class ProjectService {
+	private ProjectDao projectDao = new ProjectDao();
+
+	/**
+	 * This method simply calls the DAO class to insert a project row.
+	 * 
+	 * @param project The {@link Project} object.
+	 * @return The project object with the newly generated primary key value.
+	 */
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+}
